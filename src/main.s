2ae3cfe6073b5b64d1e74d388e2d7dbecc213d99
Josh0105 <j.calo.hernandez@gmail.com>
2026-04-21 22:18:45 -0600

    Se completa menu y se trasladan funciones de lectura de consola a utils.s

--- src/main.s
+++ src/main.s
@@ -38,9 +38,15 @@
     str6: .string "6. Gauss\n"
     str7: .string "7. Gauss-Jordan\n"
     str8: .string "8. Matriz inversa\n"
-    str9: .string "9. Funciones aritméticas con matrices\n"
-    str10: .string "10. Salir\n"
+    str9: .string "9. Determinante\n"
+    str10: .string "10. Funciones aritméticas con matrices\n"
+    str11: .string "11. Salir\n"
     errMsg: .string "Opción no válida, intente de nuevo.\n"
+    str1_1: .string "1. Suma de matrices\n"
+    str1_2: .string "2. Resta de matrices\n"
+    str1_3: .string "3. Multiplicación de matrices\n"
+    str1_4: .string "4. División de matrices\n"
+    str1_5: .string "5. Volver al menú principal\n"
     strExit: .string "Saliendo...\n"
 
 
@@ -55,6 +61,7 @@ _start:
     bl printString
     bl printEnter
 
+// Imprimimos el menú inicial
 printInitialMenu:
     ldr x0, =strInstruction
     bl printString
@@ -79,9 +86,11 @@ printInitialMenu:
     bl printString
     ldr x0, =str10
     bl printString
+    ldr x0, =str11
+    bl printString
     bl printEnter
 
-
+    // Leemos la opción ingresada en la consola
     bl readOptionFromConsole
 
     str x0, [sp, #-16]!// Guardamos la opción ingresada en la pila
@@ -109,12 +118,14 @@ printInitialMenu:
     beq case9
     cmp x0, #10
     beq case10
+    cmp x0, #11
+    beq case11
 
+    // Si la opción no es válida, imprimimos un mensaje de error
     ldr x0, =errMsg
     bl printString
-
+    // Volvemos a imprimir el menú inicial
     b printInitialMenu
-
 
 case1:
     ldr x0, =str1
@@ -161,121 +172,75 @@ case9:
     bl printString
     b printInitialMenu
 
-case10:
+case10: // Funciones aritméticas con matrices
+    b printArithmeticMenu
+
+case11: //Salir
     ldr x0, =strExit
     bl printString
     b endProgram
 
+// imprimir el menú de operaciones aritméticas con matrices
+printArithmeticMenu:
+    bl printEnter
+    ldr x0, =str1_1
+    bl printString
+    ldr x0, =str1_2
+    bl printString
+    ldr x0, =str1_3
+    bl printString
+    ldr x0, =str1_4
+    bl printString
+    ldr x0, =str1_5
+    bl printString
+    bl printEnter
 
-/* -----------------------------------------------------
-* readOptionFromConsole:
-* x0 = devuelve el entero ingresado en consola o negativo si hubo un error
-* x1 = dirección del buffer de entrada
-* x2 = tamaño del buffer de entrada
-* x8 = número de syscall (63)
-* ----------------------------------------------------- */
-readOptionFromConsole:
-  stp fp, lr, [sp, #-0x10]!
-  mov fp, sp
+//SWITCH CASE PARA LAS OPCIONES SEGUNDO MENU
+    bl readOptionFromConsole
 
-  bl cleanUpInput
-  mov x0, 0 // preparar para leer de stdin
-  ldr x1, =input
-  mov x2, 32 
-  mov x8, 63 // syscall read
-  svc 0 // ejecuta lectura
-  ldr x0, =input // cargar dirección del buffer en x0 para validación
+    str x0, [sp, #-16]!// Guardamos la opción ingresada en la pila
+    bl printEnter
+    ldr x0, [sp], #16 // Recuperamos la opción ingresada de la pila
 
-loopValidationInput:
-  ldrb w1, [x0], #1 // leemos byte a byte
-  cmp w1, 0x0 // null
-  beq endLoopValidationInput // si es null, terminamos validación
-  cmp w1, #10 // nueva línea
-  beq loopValidationInput // si es nueva línea, la ignoramos y seguimos validando
-  cmp w1, #48 // '0'
-  blt errorInputAskInteger // si es menor que '0', es inválido
-  cmp w1, #57 // '9'
-  bgt errorInputAskInteger // si es mayor que '9', es inválido
-  b loopValidationInput
+    cmp x0, #1
+    beq case1_1
+    cmp x0, #2
+    beq case1_2
+    cmp x0, #3
+    beq case1_3
+    cmp x0, #4
+    beq case1_4
+    cmp x0, #5
+    beq case1_5
+    // Si la opción no es válida, imprimimos un mensaje de error
+    ldr x0, =errMsg
+    bl printString
+    // Volvemos a imprimir el menú de operaciones aritméticas
+    b printArithmeticMenu
 
-endLoopValidationInput:
-  ldr x0, =input // cargar dirección del buffer en x0
-  mov x1, #10 // caracter de nueva línea para indicar el final de la cadena
-  bl funcAtoiWithCounter
+case1_1:
+    ldr x0, =str1_1
+    bl printString
+    b printArithmeticMenu
 
-  ldp fp, lr, [sp], #0x10
-  ret
+case1_2:
+    ldr x0, =str1_2
+    bl printString
+    b printArithmeticMenu
 
-/* -----------------------------------------------------
-* errorInputAskInteger:
-* devuelve -1 en x0 para indicar que hubo un error en la entrada del entero
-* ----------------------------------------------------- */
-errorInputAskInteger:
-  mov x0, #-1
-  ldp fp, lr, [sp], #0x10
-  ret
+case1_3:
+    ldr x0, =str1_3
+    bl printString
+    b printArithmeticMenu
 
+case1_4:
+    ldr x0, =str1_4
+    bl printString
+    b printArithmeticMenu
 
-/* -----------------------------------------------------
-* cleanUpInput:
-* limpia el buffer de entrada para evitar residuos de entradas anteriores
-* ----------------------------------------------------- */
-cleanUpInput:
-  stp fp, lr, [sp, #-0x10]!
-  mov fp, sp
+case1_5:
+    b printInitialMenu
 
-  ldr x1, =input
-  // escribimos ceros cada 8 bytes para limpiar el buffer
-  str xzr, [x1], #8
-  str xzr, [x1], #8
-  str xzr, [x1], #8
-  str xzr, [x1]
-
-  ldp fp, lr, [sp], #0x10
-  ret
-
-/* -----------------------------------------------------
-* funcAtoiWithCounter:
-* Convierte una cadena de caracteres numéricos en un entero, deteniéndose al encontrar un carácter no numérico o el final de la cadena.
-* x0 = valor entero resultante de la conversión
-* x1 = dirección de la cadena a convertir
-* x8 = caracter de nueva línea que indica el final de la cadena
-* x9 = contador de dígitos procesados
-* w2 = byte actual leído de la cadena
-* w8 = caracter de nueva línea (fin de cadena)
-* ----------------------------------------------------- */
-funcAtoiWithCounter: //x0 address string x1 the endtocount- returns x0 integer
-  stp fp, lr, [sp, #-0x10]!
-  mov fp, sp
-  mov x8, x1
-  mov x1, x0 //x1 adress
-  mov x0, #0 //value
-  mov x9, #0 //the counter
-
-processFuncAtoiWithCounter:
-  ldrb w2, [x1], #1
-  cmp w2, w8 // comparamos con el caracter de nueva línea
-  bne isNotEndLine // si no es nueva línea, seguimos procesando
-
-  mov x1, x9 // si es nueva línea, movemos el contador a x1 para devolverlo
-  ldp fp, lr, [sp], #0x10
-  ret
-
-/* -----------------------------------------------------
-* isNotEndLine:
-* Procesa cada dígito de la cadena, actualizando el valor entero acumulado en x0 y el contador de dígitos en x9.
-* x0 = valor entero acumulado
-* w2 = byte actual leído de la cadena
-* x4 = constante 10 para multiplicar el valor acumulado
-* x9 = contador de dígitos procesados
-* ----------------------------------------------------- */
-isNotEndLine:
-  add x9, x9, #1 // incrementamos el contador de dígitos
-  sub w2, w2, #48 // convertimos el carácter ASCII a su valor numérico restando 48
-  mov x4, #10 // constante para multiplicar el valor acumulado
-  mul x0, x0, x4 // multiplicamos el valor acumulado por 10 para desplazarlo a la izquierda
-  add x0, x0, x2 // sumamos el valor del dígito actual al valor acumulado
-  b processFuncAtoiWithCounter
 
 /* -----------------------------------------------------
 * syscall exit(0)
--- src/utils.s
+++ src/utils.s
@@ -7,6 +7,7 @@
 
 .global printEnter
 .global printString
+.global readOptionFromConsole
 
 /* ---------------------------------------------------------
  * Seccion bss para variables globales no inicializadas
@@ -84,3 +85,113 @@ printString: //x0 string address to print
     svc 0
     ldp fp, lr, [sp], #0x10
     ret
+
+/* -----------------------------------------------------
+* readOptionFromConsole:
+* x0 = devuelve el entero ingresado en consola o negativo si hubo un error
+* x1 = dirección del buffer de entrada
+* x2 = tamaño del buffer de entrada
+* x8 = número de syscall (63)
+* ----------------------------------------------------- */
+readOptionFromConsole:
+  stp fp, lr, [sp, #-0x10]!
+  mov fp, sp
+
+  bl cleanUpInput
+  mov x0, 0 // preparar para leer de stdin
+  ldr x1, =input
+  mov x2, 32 
+  mov x8, 63 // syscall read
+  svc 0 // ejecuta lectura
+  ldr x0, =input // cargar dirección del buffer en x0 para validación
+
+loopValidationInput:
+  ldrb w1, [x0], #1 // leemos byte a byte
+  cmp w1, 0x0 // null
+  beq endLoopValidationInput // si es null, terminamos validación
+  cmp w1, #10 // nueva línea
+  beq loopValidationInput // si es nueva línea, la ignoramos y seguimos validando
+  cmp w1, #48 // '0'
+  blt errorInputAskInteger // si es menor que '0', es inválido
+  cmp w1, #57 // '9'
+  bgt errorInputAskInteger // si es mayor que '9', es inválido
+  b loopValidationInput
+
+endLoopValidationInput:
+  ldr x0, =input // cargar dirección del buffer en x0
+  mov x1, #10 // caracter de nueva línea para indicar el final de la cadena
+  bl funcAtoiWithCounter
+
+  ldp fp, lr, [sp], #0x10
+  ret
+
+/* -----------------------------------------------------
+* errorInputAskInteger:
+* devuelve -1 en x0 para indicar que hubo un error en la entrada del entero
+* ----------------------------------------------------- */
+errorInputAskInteger:
+  mov x0, #-1
+  ldp fp, lr, [sp], #0x10
+  ret
+
+
+/* -----------------------------------------------------
+* cleanUpInput:
+* limpia el buffer de entrada para evitar residuos de entradas anteriores
+* ----------------------------------------------------- */
+cleanUpInput:
+  stp fp, lr, [sp, #-0x10]!
+  mov fp, sp
+
+  ldr x1, =input
+  // escribimos ceros cada 8 bytes para limpiar el buffer
+  str xzr, [x1], #8
+  str xzr, [x1], #8
+  str xzr, [x1], #8
+  str xzr, [x1]
+
+  ldp fp, lr, [sp], #0x10
+  ret
+
+/* -----------------------------------------------------
+* funcAtoiWithCounter:
+* Convierte una cadena de caracteres numéricos en un entero, deteniéndose al encontrar un carácter no numérico o el final de la cadena.
+* x0 = valor entero resultante de la conversión
+* x1 = dirección de la cadena a convertir
+* x8 = caracter de nueva línea que indica el final de la cadena
+* x9 = contador de dígitos procesados
+* w2 = byte actual leído de la cadena
+* w8 = caracter de nueva línea (fin de cadena)
+* ----------------------------------------------------- */
+funcAtoiWithCounter: //x0 address string x1 the endtocount- returns x0 integer
+  stp fp, lr, [sp, #-0x10]!
+  mov fp, sp
+  mov x8, x1
+  mov x1, x0 //x1 adress
+  mov x0, #0 //value
+  mov x9, #0 //the counter
+
+processFuncAtoiWithCounter:
+  ldrb w2, [x1], #1
+  cmp w2, w8 // comparamos con el caracter de nueva línea
+  bne processNextChar // si no es nueva línea, seguimos procesando
+
+  mov x1, x9 // si es nueva línea, movemos el contador a x1 para devolverlo
+  ldp fp, lr, [sp], #0x10
+  ret
+
+/* -----------------------------------------------------
+* processNextChar:
+* Procesa cada dígito de la cadena, actualizando el valor entero acumulado en x0 y el contador de dígitos en x9.
+* x0 = valor entero acumulado
+* w2 = byte actual leído de la cadena
+* x4 = constante 10 para multiplicar el valor acumulado
+* x9 = contador de dígitos procesados
+* ----------------------------------------------------- */
+processNextChar:
+  add x9, x9, #1 // incrementamos el contador de dígitos
+  sub w2, w2, #48 // convertimos el carácter ASCII a su valor numérico restando 48
+  mov x4, #10 // constante para multiplicar el valor acumulado
+  mul x0, x0, x4 // multiplicamos el valor acumulado por 10 para desplazarlo a la izquierda
+  add x0, x0, x2 // sumamos el valor del dígito actual al valor acumulado
+  b processFuncAtoiWithCounter
